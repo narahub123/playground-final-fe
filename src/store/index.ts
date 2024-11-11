57648b89cae6ff452e6490f9e3eafc0e39ad36c9
nara <parknara0213@gmail.com>
2024-11-11 14:43:59 +0900

    Feat: [추가] redux 설정 추가 - 직렬화 불가능한 데이터에 대한 상태 저장 허용
    Refs: #3

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
-export default configureStore({
+export const store = configureStore({
   reducer: {},
+
+  // serializableCheck 옵션을 false로 설정하여
+  // 직렬화 불가능한 데이터(예: 함수, class 인스턴스 등)의 상태 저장 검사를 비활성화
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+// RootState 타입 정의: store의 상태 트리 전체의 타입을 반환
+// 컴포넌트에서 useSelector 훅 등을 사용할 때 유용
+export type RootState = ReturnType<typeof store.getState>;
+// AppDispatch 타입 정의: Redux의 dispatch 함수 타입을 반환
+// 컴포넌트에서 useDispatch 훅 등을 사용할 때 유용
+export type AppDispatch = typeof store.dispatch;
+// useDispatch에 AppDispatch 타입을 적용한 커스텀 훅 생성
+// 컴포넌트에서 이 커스텀 훅을 사용해 타입 안정성이 보장된 dispatch 사용 가능
+// 사용시 const dispatch = useAppDispatch()로 사용
+export const useAppDispatch = () => useDispatch<AppDispatch>();
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [ // 경로 별칭 설정 
+    alias: [
+      // 경로 별칭 설정
       { find: "@asset", replacement: "/src/assets" },
       { find: "@routers", replacement: "/src/routers" },
       { find: "@store", replacement: "/src/store" },
