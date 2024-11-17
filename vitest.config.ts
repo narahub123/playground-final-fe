8a1607b01f580896b2bdc396f2693a88b9d0c70a
nara <parknara0213@gmail.com>
2024-11-17 20:49:35 +0900

    Fix: [수정] vitest에서 절대 경로를 인식하지 못하는 문제
    - vitest.config.ts에도 vite.config.ts와 동일하게 절대 경로를 설정해주어야 인식할 수 있는 듯
    
    Refs: #17

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@app", replacement: "/src/app" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@features", replacement: "/src/features" },
+      { find: "@shared", replacement: "/src/shared" },
+      { find: "#icons", replacement: "/src/shared/@common/ui/icons" },
+      { find: "@", replacement: "/src" },
+      { find: "@layouts", replacement: "/src/layouts" },
+      { find: "@asset", replacement: "/src/assets" },
+      { find: "@data", replacement: "/src/data" },
+      { find: "@store", replacement: "/src/store" },
+      { find: "@constants", replacement: "/src/constants" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@tests", replacement: "/src/tests" },
+    ],
+  },
   test: {
     environment: "jsdom",
     globals: true, // 글로벌 변수를 사용할 수 있도록 설정
