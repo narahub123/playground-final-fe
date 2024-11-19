52d095d1e35215d34be149681a76908cf34cbd1b
nara <parknara0213@gmail.com>
2024-11-19 13:38:44 +0900

    Fix: [에러 수정] 모달창을 여는 버튼을 눌러도 모달창이 열리지 않는 문제
    문제 파악
    - useClickOutside 내의 handleClickOutside가 모달창이 열리기 전에 호출이 되어서 문제 발생
    
    문제 해결
    - useClickOutside가 모달창이 열였을 때만 호출이 가능하도록 변경함
    
    Refs: #22

--- src/shared/@common/model/hooks/useClickOutside/useClickOutside.tsx
+++ src/shared/@common/model/hooks/useClickOutside/useClickOutside.tsx
@@ -1,16 +1,14 @@
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 
 const useClickOutside = (
   containerRef: React.RefObject<HTMLElement | null>,
   setToggle: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
-  useLayoutEffect(() => {
+  if (!containerRef.current) return;
+
+  useEffect(() => {
     // 클릭한 위치를 찾아내서 모달 내부인 여부 확인하는 함수
     const handleClickOutside = (e: MouseEvent) => {
-      console.log("handleClickOutside 호출됨");
-      e.preventDefault();
-      e.stopPropagation();
-
       const target = e.target as HTMLElement;
 
       // 클릭한 곳이 모달 창 내부가 아니라면 모달 창을 닫음
