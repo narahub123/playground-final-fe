3480e9615e40f4bec0031cc6a3fb577487bf1979
nara <parknara0213@gmail.com>
2024-11-23 16:03:07 +0900

    Fix: [수정] 불필요한 코드 삭제
    Refs: #28

--- src/shared/@common/ui/components/index.ts
+++ src/shared/@common/ui/components/index.ts
@@ -1,6 +1,5 @@
 import Icon from "./Icon/Icon";
 import ListModal from "./ListModal/ListModal";
-import FocusTrap from "./FocusTrap/FocusTrap";
 import Overlay from "./Overlay/Overlay";
 
-export { Icon, ListModal, FocusTrap, Overlay };
+export { Icon, ListModal, Overlay };
