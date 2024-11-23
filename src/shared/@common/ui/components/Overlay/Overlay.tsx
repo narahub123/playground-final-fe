2dc509108c11c458aa09710a5721e7a9324f2ac6
nara <parknara0213@gmail.com>
2024-11-23 14:43:58 +0900

    Fix: [수정] isValidColor에 설명 추가함
    Refs: #31

--- src/shared/@common/ui/components/Overlay/Overlay.tsx
+++ src/shared/@common/ui/components/Overlay/Overlay.tsx
@@ -4,9 +4,12 @@ type OverlayProp = {
 
 // 입력된 색상의 유효성 검사
 const isValidColor = (color: string): boolean => {
+  // <option> 태그의 인스턴스를 생성하고 style 객체를 가져옴
   const s = new Option().style;
+  // css의 color 속성에 입력된 color 삽입
   s.color = color;
 
+  // 삽입된 color가 유효하지 않은 값이면 빈문자열로 설정됨
   return s.color !== "";
 };
 
