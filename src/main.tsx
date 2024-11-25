412ae3c042c2395ac9091450d10c587bd226b36e
nara <parknara0213@gmail.com>
2024-11-25 15:58:35 +0900

    Feat:[추가] globals.css, reset.css 생성하기
    Refs: #38

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
-import "./index.css";
+import "./shared/@common/styles/reset.css";
+import "./shared/@common/styles/globals.css";
+import "./shared/@common/styles/theme.css";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
