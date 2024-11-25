3c91fd7cb7016ae0c8f7cb3bda5ac85093a6356c
nara <parknara0213@gmail.com>
2024-11-25 13:33:21 +0900

    Feat" [추가] 디자인 변경
    Refs: #37

--- src/shared/@common/ui/components/ToggleButton/ToggleButton.tsx
+++ src/shared/@common/ui/components/ToggleButton/ToggleButton.tsx
@@ -5,13 +5,24 @@ import styles from "./ToggleButton.module.css";
 const ToggleButton = () => {
   const [isOn, setIsOn] = useState(false);
   return (
-    <div className={styles.toggle}>
+    <div
+      className={styles.toggle}
+      style={{
+        backgroundColor: `${
+          isOn ? "rgba(100, 149, 237, 0.5)" : "rgba(100, 149, 237, 0.5)"
+        }`,
+      }}
+    >
       <div className={styles.track} onClick={() => setIsOn(!isOn)}>
         <span
           className={styles.btn}
-          style={{ left: `${isOn ? "-15px" : "32px"}` }}
+          style={{
+            left: `${isOn ? "29px" : "-14px"}`,
+            backgroundColor: `${isOn ? "rgb(100, 149, 237)" : "white"}`,
+            color: `${isOn ? "yellow" : "rgb(100, 149, 237)"}`,
+          }}
         >
-          <Icon iconName="close" iconTitle="" />
+          <Icon iconName={isOn ? "light" : "dark"} iconTitle="" fontSize={20} />
         </span>
       </div>
     </div>
--- src/shared/@common/ui/icons/index.ts
+++ src/shared/@common/ui/icons/index.ts
@@ -1,10 +1,13 @@
 import { FaArrowLeft as backward } from "react-icons/fa6";
 import { LuX as close } from "react-icons/lu";
+import { CiLight as light, CiDark as dark } from "react-icons/ci";
 
 const ICONS = {
   backward, // 뒤로 가기
   wrong: close, // 없는 아이콘 이름을 넣는 경우
   close, // 창 닫기
+  dark, // 다크 모드
+  light, // 라이트 모드
 };
 
 export default ICONS;
