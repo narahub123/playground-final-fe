37f556068d036f33f39ab6e5c92cb2aff1b98b9c
nara <parknara0213@gmail.com>
2024-11-26 16:22:18 +0900

    Feat: [추가] Input이 포커스인 경우 input요소로 포커스 이동하기
    Refs: #45

--- src/shared/@common/ui/components/Input/Input.tsx
+++ src/shared/@common/ui/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Input.module.css";
 import Icon from "../Icon/Icon";
 
@@ -21,6 +21,7 @@ const Input = ({
   iconColor,
   iconHandleClick,
 }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [isFocus, setIsFocus] = useState(false);
   // 비밀번호 보이기 여부 상태
   const [isVisible, setIsVisible] = useState(false);
@@ -28,6 +29,15 @@ const Input = ({
   const [isValid, setIsValid] = useState(true);
   const [value, setValue] = useState("");
   const focusCond = isFocus || value ? styles.focused : "";
+
+  useEffect(() => {
+    if (!isFocus) return;
+    if (!inputRef.current) return;
+
+    if (isFocus) {
+      inputRef.current.focus();
+    }
+  }, [isFocus]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -59,6 +69,7 @@ const Input = ({
           type={inputType}
           className={`${styles.input} ${focusCond}`}
           onChange={(e) => handleChange(e)}
+          ref={inputRef}
         />
         {field === "password" && (
           <Icon
