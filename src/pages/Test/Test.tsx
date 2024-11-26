1856b6cbff64a6bb3d257da5caacd6e298d1fd75
nara <parknara0213@gmail.com>
2024-11-26 15:18:58 +0900

    Fix: [수정] upper 너비 수정
    Refs: #41

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -144,6 +144,7 @@ const Test = () => {
       <div className={styles[`input-container`]}>
         <Input field="username" fieldTitle="이름" valueMaxLength={20} />
         <Input field="password" fieldTitle="비밀번호" />
+        <Input field="userId" fieldTitle="사용자 아이디" valueMaxLength={20} />
       </div>
     </div>
   );
