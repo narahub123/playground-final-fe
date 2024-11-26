898cf12da2d8c784d067e224224cee12243f3512
nara <parknara0213@gmail.com>
2024-11-26 18:12:51 +0900

    Feat: [추가] 유효하지 않은 경우 에러 메시지 추가 +접근성 추가
    Refs: #40

--- src/shared/@common/ui/components/Input/Input.tsx
+++ src/shared/@common/ui/components/Input/Input.tsx
@@ -11,6 +11,7 @@ interface InputProps {
   iconTitle?: string;
   iconColor?: string;
   iconHandleClick?: () => void;
+  errorMessage?: string;
 }
 
 const Input = ({
@@ -21,6 +22,7 @@ const Input = ({
   iconTitle = "",
   iconColor,
   iconHandleClick,
+  errorMessage,
 }: InputProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -28,9 +30,10 @@ const Input = ({
   // 비밀번호 보이기 여부 상태
   const [isVisible, setIsVisible] = useState(false);
   // 입력값의 유효성을 여부 상태
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(false);
   const [value, setValue] = useState("");
   const focusCond = isFocus || value ? styles.focused : "";
+  const validCond = !isValid && value ? styles.invalid : "";
 
   useEffect(() => {
     if (!isFocus || !inputRef.current) return;
@@ -54,59 +57,71 @@ const Input = ({
   const inputType = field === "password" && !isVisible ? "password" : "text";
 
   return (
-    <div
-      className={`${styles.container} ${focusCond}`}
-      onClick={() => {}}
-      tabIndex={isFocus ? -1 : 0}
-      onFocus={() => setIsFocus(true)}
-      onBlur={(e) => {
-        e.stopPropagation();
-        setIsFocus(false);
-      }}
-      ref={containerRef}
-    >
-      <div className={`${styles.upper} ${focusCond}`}>
-        <span className={`${styles.title} ${focusCond}`}>{fieldTitle}</span>
-        <span
-          className={`${styles.count} ${valueMaxLength ? focusCond : ""}`}
-        >{`${value.length} / ${valueMaxLength}`}</span>
+    <div className={styles.wrapper}>
+      <div
+        className={`${styles.container} ${focusCond} ${validCond}`}
+        onClick={() => {}}
+        tabIndex={isFocus ? -1 : 0}
+        onFocus={() => setIsFocus(true)}
+        onBlur={(e) => setIsFocus(false)}
+        ref={containerRef}
+      >
+        <div className={`${styles.upper} ${focusCond}`}>
+          <label htmlFor={field} className={`${styles.title} ${focusCond}`}>
+            {fieldTitle}
+          </label>
+          <span
+            className={`${styles.count} ${valueMaxLength ? focusCond : ""}`}
+          >{`${value.length} / ${valueMaxLength}`}</span>
+        </div>
+        <div className={`${styles.lower} ${focusCond}`}>
+          <input
+            id={field}
+            type={inputType}
+            className={`${styles.input} ${focusCond}`}
+            onChange={debounceHandleChange}
+            ref={inputRef}
+            aria-labelledby={field}
+            aria-invalid={!isValid}
+            aria-describedby={isValid ? "" : "input-error"}
+          />
+          {field === "password" && (
+            <Icon
+              iconName={isVisible ? "eyeoff" : "eye"}
+              iconTitle={isVisible ? "비밀번호 숨기기" : "비밀번호 보이기"}
+              handleClick={() => {
+                setIsVisible(!isVisible);
+              }}
+              className={`${styles.icon} ${focusCond}`}
+            />
+          )}
+          {field === "userId" && (
+            <Icon
+              iconName={isValid ? "valid" : "invalid"}
+              iconTitle={isValid ? "입력값이 유효함" : "입력값이 유효하지 않음"}
+              className={`${styles.icon} ${focusCond}`}
+              color={isValid ? "green" : "red"}
+              ariaHidden="true"
+            />
+          )}
+          {iconName && (
+            <Icon
+              iconName={iconName}
+              iconTitle={iconTitle}
+              className={`${styles.icon} ${focusCond}`}
+              color={iconColor}
+              handleClick={iconHandleClick}
+            />
+          )}
+        </div>
       </div>
-      <div className={`${styles.lower} ${focusCond}`}>
-        <input
-          type={inputType}
-          className={`${styles.input} ${focusCond}`}
-          onChange={debounceHandleChange}
-          ref={inputRef}
-        />
-        {field === "password" && (
-          <Icon
-            iconName={isVisible ? "eyeoff" : "eye"}
-            iconTitle={isVisible ? "비밀번호 숨기기" : "비밀번호 보이기"}
-            handleClick={() => {
-              setIsVisible(!isVisible);
-            }}
-            className={`${styles.icon} ${focusCond}`}
-          />
-        )}
-        {field === "userId" && (
-          <Icon
-            iconName={isValid ? "valid" : "invalid"}
-            iconTitle={isValid ? "입력값이 유효함" : "입력값이 유효하지 않음"}
-            className={`${styles.icon} ${focusCond}`}
-            color={isValid ? "green" : "red"}
-            ariaHidden="true"
-          />
-        )}
-        {iconName && (
-          <Icon
-            iconName={iconName}
-            iconTitle={iconTitle}
-            className={`${styles.icon} ${focusCond}`}
-            color={iconColor}
-            handleClick={iconHandleClick}
-          />
-        )}
-      </div>
+      {validCond && (
+        <div className={styles.error}>
+          <p id="input-error" className={styles[`error-message`]}>
+            {errorMessage || "입력값이 유효하지 않습니다."}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
