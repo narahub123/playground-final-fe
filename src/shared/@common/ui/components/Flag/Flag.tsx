ae024b82e86926d4163661871acb62369105c7b0
nara <parknara0213@gmail.com>
2024-11-27 15:06:47 +0900

    Feat: [추가] 이미지 에러 처리하기 - 잘못된 국가 코드가 입력된 경우 - 로딩에 실패한 경우
    Refs: #49

--- src/shared/@common/ui/components/Flag/Flag.tsx
+++ src/shared/@common/ui/components/Flag/Flag.tsx
@@ -7,10 +7,13 @@ type FlagProps = {
 };
 
 const Flag = ({ countryCode, width = 20 }: FlagProps) => {
-  // 국기 이미지
-  const flag = flags[countryCode as keyof typeof flags];
+  // 국기 이미지: countryCode가 없는 경우 un 깃발이 보이도록 함
+  const flag = flags[countryCode as keyof typeof flags] || flags[`un`];
+
   // 국가 이름
-  const flagAlt = countryNames[countryCode as keyof typeof countryNames];
+  const flagAlt =
+    countryNames[countryCode as keyof typeof countryNames] ||
+    "잘못된 국가 코드 입력";
 
   return (
     <img
@@ -21,6 +24,9 @@ const Flag = ({ countryCode, width = 20 }: FlagProps) => {
       alt={`${flagAlt}의 국기 이미지`} // 대체 텍스트
       aria-hidden="true" // 의미 없는 콘턴츠임
       aria-label={`${flagAlt}의 국기`} // 이미지 설명을 명시적으로 지정
+      onError={(e) =>
+        (e.currentTarget.src = "/src/shared/@common/asset/un.svg")
+      } // 이미지 로딩에 실패한 경우
     />
   );
 };
