c3e7a2f6e8c6f37ba5f7ab90885945d6fcedbbc7
nara <parknara0213@gmail.com>
2024-11-29 12:05:43 +0900

    Fix: [수정] 에러 메시지 렌더링 방법 수정
    Refs: #47

--- src/shared/@common/ui/components/Textarea/Textarea.tsx
+++ src/shared/@common/ui/components/Textarea/Textarea.tsx
@@ -1,83 +1,85 @@
-import { useEffect, useRef, useState } from "react";
-import styles from "./Textarea.module.css";
-import { debounce } from "@shared/@common/utils";
+  import { useEffect, useRef, useState } from "react";
+  import styles from "./Textarea.module.css";
+  import { debounce } from "@shared/@common/utils";
 
-interface TextareaProps {
-  field: string;
-  fieldTitle: string;
-  valueMaxLength?: number;
-  errorMessage?: string;
-  disabled?: boolean;
-}
+  interface TextareaProps {
+    field: string;
+    fieldTitle: string;
+    valueMaxLength?: number;
+    errorMessage?: string;
+    disabled?: boolean;
+  }
 
-const Textarea = ({
-  field,
-  fieldTitle,
-  valueMaxLength,
-  errorMessage,
-  disabled = false,
-}: TextareaProps) => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const [isFocused, setIsFocused] = useState(false);
-  const [value, setValue] = useState("");
-  const [isValid, setIsValid] = useState(false);
+  const Textarea = ({
+    field,
+    fieldTitle,
+    valueMaxLength,
+    errorMessage,
+    disabled = false,
+  }: TextareaProps) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [value, setValue] = useState("");
+    const [isValid, setIsValid] = useState(false);
 
-  useEffect(() => {
-    if (isFocused) {
-      textareaRef.current?.focus();
-    }
-  }, [isFocused]);
+    useEffect(() => {
+      if (isFocused) {
+        textareaRef.current?.focus();
+      }
+    }, [isFocused]);
 
-  const focusCond = isFocused || value !== "" ? styles.focused : "";
-  const validCond = !isValid && value !== "" ? styles.invalid : "";
+    const focusCond = isFocused || value !== "" ? styles.focused : "";
+    const validCond = !isValid && value !== "" ? styles.invalid : "";
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const value = e.target.value;
 
-    setValue(value);
+      setValue(value);
+    };
+
+    const debounceHandleChange = debounce<typeof handleChange>(handleChange, 500);
+    return (
+      <div className={styles.wrapper}>
+        <div
+          className={`${styles.container} ${focusCond} ${validCond}`}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          tabIndex={isFocused || disabled ? -1 : 0}
+        >
+          <div className={`${styles.upper} ${focusCond}`}>
+            <label htmlFor={field} className={`${styles.title} ${focusCond}`}>
+              {fieldTitle}
+            </label>
+            <span
+              className={`${styles.count} ${focusCond}`}
+            >{`${value.length} / ${valueMaxLength}`}</span>
+          </div>
+          <div className={`${styles.lower} ${focusCond}`}>
+            <textarea
+              id={field}
+              className={`${styles.textarea} ${focusCond}`}
+              onChange={debounceHandleChange}
+              ref={textareaRef}
+              aria-labelledby={field}
+              aria-invalid={!isValid}
+              aria-describedby={isValid ? "" : "textarea-error"}
+              disabled={disabled}
+              aria-hidden={disabled}
+            />
+          </div>
+        </div>
+        {validCond && (
+          <div className={styles.error}>
+            <p
+              id="textarea-error"
+              className={`${styles[`error-message`]} ${validCond}`}
+            >
+              {errorMessage || "입력값이 유효하지 않습니다."}
+            </p>
+          </div>
+        )}
+      </div>
+    );
   };
 
-  const debounceHandleChange = debounce<typeof handleChange>(handleChange, 500);
-  return (
-    <div className={styles.wrapper}>
-      <div
-        className={`${styles.container} ${focusCond} ${validCond}`}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        tabIndex={isFocused || disabled ? -1 : 0}
-      >
-        <div className={`${styles.upper} ${focusCond}`}>
-          <label htmlFor={field} className={`${styles.title} ${focusCond}`}>
-            {fieldTitle}
-          </label>
-          <span
-            className={`${styles.count} ${focusCond}`}
-          >{`${value.length} / ${valueMaxLength}`}</span>
-        </div>
-        <div className={`${styles.lower} ${focusCond}`}>
-          <textarea
-            id={field}
-            className={`${styles.textarea} ${focusCond}`}
-            onChange={debounceHandleChange}
-            ref={textareaRef}
-            aria-labelledby={field}
-            aria-invalid={!isValid}
-            aria-describedby={isValid ? "" : "textarea-error"}
-            disabled={disabled}
-            aria-hidden={disabled}
-          />
-        </div>
-      </div>
-      <div className={styles.error}>
-        <p
-          id="textarea-error"
-          className={`${styles[`error-message`]} ${validCond}`}
-        >
-          {errorMessage || "입력값이 유효하지 않습니다."}
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default Textarea;
+  export default Textarea;
