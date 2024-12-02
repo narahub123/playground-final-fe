b790ab7024db43dd7f92eb2fd26088aa92a1fdb8
nara <parknara0213@gmail.com>
2024-12-02 18:03:29 +0900

    Feat: [추가] 접근성 코드 추가
    Refs: #64

--- src/shared/@common/ui/components/Image/Image.tsx
+++ src/shared/@common/ui/components/Image/Image.tsx
@@ -56,6 +56,10 @@ const Image = ({
       },
       onFocus: () => setIsFocused(true),
       onBlur: () => setIsFocused(false),
+      ...(isFocusable
+        ? { "aria-label": `${to}로 이동` }
+        : { "aria-labelledby": "figure-description" }),
+      "aria-live": isFocused ? "polite" : "off",
     },
 
     child
