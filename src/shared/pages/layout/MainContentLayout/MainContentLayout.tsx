46294c0c5cb6242d7b131aed31969553f2b3b5c3
nara <parknara0213@gmail.com>
2024-12-04 12:43:44 +0900

    Feat: [추가] 상,하 컨턴츠가 공유하는 정보를 다룰 상태 생성
    Refs: #72

--- src/shared/pages/layout/MainContentLayout/MainContentLayout.tsx
+++ src/shared/pages/layout/MainContentLayout/MainContentLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 import styles from "./MainContentLayout.module.css";
 
 interface MainContentLayoutProps {
@@ -10,10 +10,23 @@ const MainContentLayout = ({
   topContent,
   bottomContent,
 }: MainContentLayoutProps) => {
+  // 검색어 상태
+  const [search, setSearch] = useState("");
+
+  // 공유하는 정보를 컴포넌트에 추가함
+  const top = React.cloneElement(topContent as ReactElement, {
+    search,
+    setSearch,
+  });
+
+  const bottom = React.cloneElement(bottomContent as ReactElement, {
+    search,
+  });
+
   return (
     <div className={styles.layout}>
-      <div className={styles.topContent}>{topContent}</div>
-      <div className={styles.bottomContent}>{bottomContent}</div>
+      <div className={styles.topContent}>{top}</div>
+      <div className={styles.bottomContent}>{bottom}</div>
     </div>
   );
 };
