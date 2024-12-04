ee8ade9a0a16daf1c008def40c08b4575d7050a7
nara <parknara0213@gmail.com>
2024-12-04 17:45:20 +0900

    Feat: [추가] 포커스 트랩
    Refs: #76

--- src/shared/pages/ui/Tab/Tab.tsx
+++ src/shared/pages/ui/Tab/Tab.tsx
@@ -1,6 +1,6 @@
 import { TabItemType } from "@shared/pages/types";
 import styles from "./Tab.module.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface TabProps {}
@@ -20,13 +20,28 @@ const Tab = ({}: TabProps) => {
     },
   ];
 
+  const itemRefs = useRef<any>([]);
   const [selection, setSelection] = useState(list[0].text);
+  const [focus, setFocus] = useState("");
+
+  useEffect(() => {
+    const index = list.findIndex((item) => item.text === focus);
+
+    if (index !== -1) {
+      itemRefs.current[index].focus();
+    }
+  }, [focus]);
 
   return (
     <div className={styles.tab}>
       <ul className={styles.list}>
-        {list.map((item) => {
+        {list.map((item, index) => {
           const selectedCond = selection === item.text ? styles.selected : "";
+
+          // -> 키에 대한 조건
+          const indexPlusCond = index + 1 === list.length ? 0 : index + 1;
+          // <- 키에 대한 조건
+          const indexMinusCond = index - 1 < 0 ? list.length - 1 : index - 1;
 
           const Item = item.path ? Link : "li";
           return (
@@ -35,6 +50,21 @@ const Tab = ({}: TabProps) => {
               className={styles.item}
               to={item.path || ""}
               onClick={() => setSelection(item.text)}
+              onFocus={() => setFocus(item.text)}
+              onBlur={() => setFocus("")}
+              // tab 이동
+              tabIndex={selection === item.text ? 0 : -1}
+              ref={(el: any) => {
+                itemRefs.current[index] = el;
+              }}
+              // 방향키로 포커스 이동
+              onKeyDown={(e) => {
+                if (e.key === "ArrowRight") {
+                  setFocus(list[indexPlusCond].text);
+                } else if (e.key === "ArrowLeft") {
+                  setFocus(list[indexMinusCond].text);
+                }
+              }}
             >
               <div className={`${styles.wrap} ${selectedCond}`}>
                 <span className={styles.text}>{item.text}</span>
