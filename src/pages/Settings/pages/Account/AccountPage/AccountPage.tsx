3846328f850289cb198941578e25009cdda3989d
nara <parknara0213@gmail.com>
2024-12-07 00:08:26 +0900

    Feat: [추가] 언어 설정
    Refs: #84

--- src/pages/Settings/pages/Account/AccountPage/AccountPage.tsx
+++ src/pages/Settings/pages/Account/AccountPage/AccountPage.tsx
@@ -1,14 +1,14 @@
 import { SettingsBranchCard } from "@features/settings-setting/ui/components";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { Description } from "@shared/@common/ui/components";
 import { MainContentListLayout, MainLayout } from "@shared/pages/layout";
 
 const AccountPage = () => {
+  const { pageTitle, description } = useLanguageMode(["pages", "AccountPage"]);
   return (
     <MainLayout
-      pageTitle="계정"
-      topContent={
-        <Description text="계정 정보를 확인하고, 데이터 기록을 다운로드하거나, 계정 비활성화 옵션에 대해 자세히 알아보세요." />
-      }
+      pageTitle={pageTitle}
+      topContent={<Description text={description} />}
       bottomContent={
         <MainContentListLayout
           listName={["accountBranchList"]}
