688f162cab213e9774c40bfe71b0644a5bc52da3
nara <parknara0213@gmail.com>
2024-12-06 13:02:04 +0900

    Feat: [추가] 언어 설정
    Refs: #83

--- src/pages/Settings/pages/SettingsLandingPage/SettingsLandingPage.tsx
+++ src/pages/Settings/pages/SettingsLandingPage/SettingsLandingPage.tsx
@@ -1,12 +1,19 @@
 import { SettingsBranchCard } from "@features/settings-setting/ui/components";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { MainContentListLayout, MainLayout } from "@shared/pages/layout";
 import { Search } from "@shared/pages/ui";
 
 const SettingsLandingPage = () => {
+  // 언어 설정
+  const { pageTitle, placeholder } = useLanguageMode([
+    "pages",
+    "settingsLandingPage",
+  ]);
+
   return (
     <MainLayout
-      pageTitle="설정"
-      topContent={<Search placeholder="설정 검색하기" />}
+      pageTitle={pageTitle}
+      topContent={<Search placeholder={placeholder} />}
       bottomContent={
         <MainContentListLayout
           listName={["settingsMainBranchList"]}
