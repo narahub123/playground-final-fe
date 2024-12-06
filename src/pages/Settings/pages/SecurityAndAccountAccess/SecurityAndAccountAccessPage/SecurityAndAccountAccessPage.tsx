8087c0e11dd8cb3be1db1c7e9426da3499f3d1c5
nara <parknara0213@gmail.com>
2024-12-07 00:30:47 +0900

    Feat: [추가] 언어 설정
    Refs: #85

--- src/pages/Settings/pages/SecurityAndAccountAccess/SecurityAndAccountAccessPage/SecurityAndAccountAccessPage.tsx
+++ src/pages/Settings/pages/SecurityAndAccountAccess/SecurityAndAccountAccessPage/SecurityAndAccountAccessPage.tsx
@@ -1,14 +1,17 @@
 import { SettingsBranchCard } from "@features/settings-setting/ui/components";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { Description } from "@shared/@common/ui/components";
 import { MainContentListLayout, MainLayout } from "@shared/pages/layout";
 
 const SecurityAndAccountAccessPage = () => {
+  const { pageTitle, description } = useLanguageMode([
+    "pages",
+    "SecurityAndAccountAccessPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="보안과 계정 접근 권한"
-      topContent={
-        <Description text="계정의 보안을 관리하고, 계정에 연결된 앱을 포함해 계정 사용을 추적합니다." />
-      }
+      pageTitle={pageTitle}
+      topContent={<Description text={description} />}
       bottomContent={
         <MainContentListLayout
           listName={["securityAndAccountAccessBranchList"]}
