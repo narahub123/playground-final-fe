8c8d6fe2a0468fe2031ede95cff701e17353e92b
nara <parknara0213@gmail.com>
2024-12-06 11:35:35 +0900

    Feat: [추가] 검색 props 추가 및 검색 결과 표시 추가
    Refs: #77

--- src/shared/pages/layout/MainContentListLayout/MainContentListLayout.tsx
+++ src/shared/pages/layout/MainContentListLayout/MainContentListLayout.tsx
@@ -6,18 +6,31 @@ import { SettingsBranchType } from "@features/settings-setting/types/data";
 interface MainContentListLayoutProps {
   listName: string;
   item: ReactNode;
+  search?: string;
 }
 
 const MainContentListLayout = ({
   listName,
   item,
+  search,
 }: MainContentListLayoutProps) => {
   const list: SettingsBranchType[] = useLanguageMode(listName);
+
+  const filteredList = list.filter((item) => {
+    if (!search) {
+      return item;
+    } else {
+      return item.title.includes(search);
+    }
+  });
 
   return (
     <div className={styles[`list-layout`]}>
       <ul className={styles.list}>
-        {list.map((i, index) => {
+        {search && filteredList.length === 0 && (
+          <>{`${search}에 대한 검색 결과가 없습니다.`}</>
+        )}
+        {filteredList.map((i, index) => {
           const Item = cloneElement(item as ReactElement, {
             item: i,
             key: index,
