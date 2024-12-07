349c497ef8e35fc224b40976cca235bf209489c2
nara <parknara0213@gmail.com>
2024-12-07 12:58:14 +0900

    Feat: [추가] 이전 페이지로 이동하는 아이콘 추가
    Refs: #88

--- src/pages/Settings/pages/AccessibilityAndDisplayAndLanguages/AccessibilityAndDisplayAndLanguagesPage/AccessibilityAndDisplayAndLanguagesPage.tsx
+++ src/pages/Settings/pages/AccessibilityAndDisplayAndLanguages/AccessibilityAndDisplayAndLanguagesPage/AccessibilityAndDisplayAndLanguagesPage.tsx
@@ -11,6 +11,7 @@ const AccessibilityAndDisplayAndLanguagesPage = () => {
   return (
     <MainLayout
       pageTitle={pageTitle}
+      backward
       topContent={<Description text={description} />}
       bottomContent={
         <MainContentListLayout
