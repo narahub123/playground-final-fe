0f387252549c2120b28a0d68ab47510f16801779
nara <parknara0213@gmail.com>
2024-12-08 02:00:41 +0900

    Fix: [수정] 불필요한 코드 삭제
    Refs: #93

--- src/pages/Settings/pages/SecurityAndAccountAccess/SecurityPage/SecurityPage.tsx
+++ src/pages/Settings/pages/SecurityAndAccountAccess/SecurityPage/SecurityPage.tsx
@@ -1,30 +1,7 @@
-import {
-  CheckBoxType,
-  SettingsBranchType,
-} from "@features/settings-setting/types/data";
-import styles from "./SecurityPage.module.css";
-import {
-  CheckBox,
-  SettingsBranchCard,
-  SettingsContainer,
-} from "@features/settings-setting/ui/components";
-import { Description, Title } from "@shared/@common/ui/components";
+import { SettingsContainer } from "@features/settings-setting/ui/components";
+import { Description } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
 import { useLanguageMode } from "@shared/@common/model/hooks";
-
-interface SettingsContainerType {
-  title: string;
-  type: string;
-  description: string;
-}
-
-interface SettingsBranchListContainerType extends SettingsContainerType {
-  comp: SettingsBranchType;
-}
-
-interface SettingsCheckBoxContainerType extends SettingsContainerType {
-  comp: CheckBoxType;
-}
 
 const SecurityPage = () => {
   const { pageTitle, description, list } = useLanguageMode([
