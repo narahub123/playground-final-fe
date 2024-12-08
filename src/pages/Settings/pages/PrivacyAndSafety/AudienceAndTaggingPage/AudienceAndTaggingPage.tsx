057a17f08e3dd567c6770cd720de5e0a87cd539f
nara <parknara0213@gmail.com>
2024-12-09 01:20:42 +0900

    Feat: [추가] 언어 설정
    Refs: #100

--- src/pages/Settings/pages/PrivacyAndSafety/AudienceAndTaggingPage/AudienceAndTaggingPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/AudienceAndTaggingPage/AudienceAndTaggingPage.tsx
@@ -1,52 +1,18 @@
 import { SettingsContainer } from "@features/settings-setting/ui/components";
-import { setBackgroundTheme } from "@shared/@common/model/slices/settingsSlice";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { Description } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
 
 const AudienceAndTaggingPage = () => {
-  const list = [
-    {
-      title: "",
-      type: "checkbox",
-      comp: {
-        text: "내 게시물을 비공개로 설정",
-        reducer: setBackgroundTheme,
-        initialValue: false,
-      },
-      description:
-        "선택하면 나를 팔로우하는 사람들만 내 게시물 및 기타 계정 정보를 볼 수 있게 됩니다.",
-    },
-    {
-      title: "",
-      type: "checkbox",
-      comp: {
-        text: "동영상 보호하기",
-        reducer: setBackgroundTheme,
-        initialValue: false,
-      },
-      description:
-        "이 설정을 선택하면 기본적으로 게시물의 동영상을 다운로드할 수 없게 됩니다. 이 설정은 앞으로 작성하는 게시물에 적용되며 소급 적용되지 않습니다. ",
-    },
-    {
-      title: "",
-      type: "card",
-      comp: {
-        title: "사진 태그하기",
-        path: "/settings/tagging",
-        expl: "누구나 나를 태그할 수 있도록 허용",
-      },
-      description: "",
-    },
-  ];
+  const { pageTitle, description, list } = useLanguageMode([
+    "pages",
+    "AudienceAndTaggingPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="오디언스, 미디어 및 태그하기"
+      pageTitle={pageTitle}
       backward
-      topContent={
-        <Description
-          text={"PlayGround에서 다른 사람들에게 공개하는 정보를 관리합니다."}
-        />
-      }
+      topContent={<Description text={description} />}
       bottomContent={<SettingsContainer list={list} />}
     />
   );
