b4890a922ef94acbdba01972a5065f914fb6b246
nara <parknara0213@gmail.com>
2024-12-08 13:30:03 +0900

    Fix: [수정] 언어 설정 변경
    Refs: #95

--- src/pages/Settings/pages/Account/AccountInfoPage/AccountInfoPage.tsx
+++ src/pages/Settings/pages/Account/AccountInfoPage/AccountInfoPage.tsx
@@ -1,12 +1,21 @@
 import { SettingsBranchCard } from "@features/settings-setting/ui/components";
 import { useLanguageMode } from "@shared/@common/model/hooks";
+import { getUser } from "@shared/@common/model/selectors";
+import { getSettings } from "@shared/@common/model/selectors/settingsSelectors";
 import { MainContentListLayout, MainLayout } from "@shared/pages/layout";
+import { useSelector } from "react-redux";
 
 const AccountInfoPage = () => {
   const { pageTitle, branchList } = useLanguageMode([
     "pages",
     "AccountInfoPage",
   ]);
+
+  const userInfo = useSelector(getUser);
+  const settingsInfo = useSelector(getSettings);
+
+  const user = { ...userInfo, ...settingsInfo };
+
   return (
     <MainLayout
       pageTitle={pageTitle}
@@ -14,7 +23,7 @@ const AccountInfoPage = () => {
       topContent={<></>}
       bottomContent={
         <MainContentListLayout
-          list={branchList}
+          list={branchList(user)}
           item={<SettingsBranchCard />}
         />
       }
