aaa28538a7993bf8cb20e443b50fa0301fe00695
nara <parknara0213@gmail.com>
2024-12-09 00:37:17 +0900

    Feat: [추가] user 정보 추가
    Refs: #97

--- src/shared/@common/ui/components/ProfileContainer/ProfileContainer.tsx
+++ src/shared/@common/ui/components/ProfileContainer/ProfileContainer.tsx
@@ -2,14 +2,18 @@ import { Link } from "react-router-dom";
 import Description from "../Description/Description";
 import ProfileImage from "../ProfileImage/ProfileImage";
 import styles from "./ProfileContainer.module.css";
+import { useSelector } from "react-redux";
+import { getUser } from "@shared/@common/model/selectors";
 
 const ProfileContainer = () => {
+  const user = useSelector(getUser);
+
   return (
-    <Link className={styles[`profile-container`]} to={"/abced"}>
+    <Link className={styles[`profile-container`]} to={`/${user.userId}`}>
       <ProfileImage size={40} />
       <span className={styles.info}>
-        <p className={styles.username}>몰러</p>
-        <Description text={`@${"abced"}`} />
+        <p className={styles.username}>{user.username}</p>
+        <Description text={`@${user.userId}`} />
       </span>
     </Link>
   );
