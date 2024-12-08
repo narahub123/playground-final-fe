59adce450d9109072a1fdce699afaa417be36ba3
nara <parknara0213@gmail.com>
2024-12-09 02:42:52 +0900

    Feat: [추가] 언어 설정
    Refs: #101

--- src/pages/Settings/pages/PrivacyAndSafety/YourPostsPage/YourPostsPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/YourPostsPage/YourPostsPage.tsx
@@ -1,36 +1,19 @@
 import { SettingsContainer } from "@features/settings-setting/ui/components";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { setBackgroundTheme } from "@shared/@common/model/slices/settingsSlice";
 import { Description } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
 
 const YourPostsPage = () => {
-  const list = [
-    {
-      title: "",
-      type: "checkbox",
-      comp: {
-        text: "게시하는 미디어를 민감한 내용이 포함될 수 있는 미디어로 표시하기",
-        reducer: setBackgroundTheme,
-        initialValue: false,
-      },
-      description:
-        "활성화하면 게시하는 사진과 동영상이 민감한 콘텐츠가 표시되기를 원치 않는 사람들에게 민감한 콘텐츠로 표시됩니다.",
-    },
-    {
-      title: "",
-      type: "card",
-      comp: {
-        title: "게시물에 위치 정보 넣기",
-        path: "/settings/location",
-      },
-      description: "",
-    },
-  ];
+  const { pageTitle, description, list } = useLanguageMode([
+    "pages",
+    "YourPostsPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="내 게시물"
+      pageTitle={pageTitle}
       backward
-      topContent={<Description text={"게시물과 관련된 정보를 관리합니다."} />}
+      topContent={<Description text={description} />}
       bottomContent={<SettingsContainer list={list} gap={0} />}
     />
   );
