a89b34af6a5130098af9b264515a267ef90ad638
nara <parknara0213@gmail.com>
2024-12-10 02:51:30 +0900

    Feat: [추가] 언어설정
    Refs: #114

--- src/pages/Settings/pages/SecurityAndAccountAccess/LoginVerficationPage/LoginVerficationPage.tsx
+++ src/pages/Settings/pages/SecurityAndAccountAccess/LoginVerficationPage/LoginVerficationPage.tsx
@@ -1,56 +1,29 @@
+import {
+  SettingsBranchListContainerType,
+  SettingsCheckBoxContainerType,
+} from "@features/settings-setting/types";
 import { SettingsContainer } from "@features/settings-setting/ui/components";
-import { getTwoFactorAuthentication } from "@shared/@common/model/selectors";
-import { setTwoFactorAuthentication } from "@shared/@common/model/slices/settingsSlice";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { MainLayout } from "@shared/pages/layout";
 
 const LoginVerficationPage = () => {
-  const list = [
-    {
-      title: "2단계 인증",
-      type: "checkbox",
-      selector: getTwoFactorAuthentication,
-      comp: {
-        text: "문자 메시지",
-        reducer: setTwoFactorAuthentication,
-        value: "sms",
-      },
-      description:
-        "PG에 로그인할 때 입력할 인증 코드가 포함된 문자 메시지를 휴대폰으로 받습니다.",
-      top: true,
-    },
-    {
-      type: "checkbox",
-      selector: getTwoFactorAuthentication,
-      comp: {
-        text: "인증 앱",
-        reducer: setTwoFactorAuthentication,
-        value: "app",
-      },
-      description:
-        "모바일 인증 앱을 사용하여 PG에 로그인할 때마다 입력할 인증 코드를 받으세요.",
-      top: true,
-    },
-    {
-      type: "checkbox",
-      selector: getTwoFactorAuthentication,
-      comp: {
-        text: "보안 키",
-        reducer: setTwoFactorAuthentication,
-        value: "key",
-      },
-      description:
-        "PG에 로그인할 때 컴퓨터에 입력하거나 모바일 디바이스와 동기화되는 보안 키를 사용하세요. 지원되는 모바일 디바이스나 웹 브라우저를 사용해야 합니다.",
-      top: true,
-    },
-  ];
+  const { pageTitle, list } = useLanguageMode([
+    "pages",
+    "LoginVerficationPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="2단계 인증"
+      pageTitle={pageTitle}
       backward
       topContent={<></>}
       bottomContent={
         <>
-          {list.map((item, index) => (
+          {(
+            list as (
+              | SettingsBranchListContainerType
+              | SettingsCheckBoxContainerType
+            )[]
+          ).map((item, index) => (
             <SettingsContainer item={item} key={index} gap={0} />
           ))}
         </>
