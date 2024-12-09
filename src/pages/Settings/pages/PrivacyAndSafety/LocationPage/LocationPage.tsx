9e0b69e59f8b8f37ab2766765faca671fa8e7718
nara <parknara0213@gmail.com>
2024-12-10 01:15:15 +0900

    Feat: [추가] 언어 설정
    Refs: #112

--- src/pages/Settings/pages/PrivacyAndSafety/LocationPage/LocationPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/LocationPage/LocationPage.tsx
@@ -1,32 +1,25 @@
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import styles from "./LocationPage.module.css";
 import { SettingsContainer } from "@features/settings-setting/ui/components";
-import { getAddLocationInfo } from "@shared/@common/model/selectors";
-import { setAddLocationInfo } from "@shared/@common/model/slices/settingsSlice";
 import { Description, HyperLink } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
 
 const LocationPage = () => {
-  const item = {
-    type: "checkbox",
-    selector: getAddLocationInfo,
-    comp: {
-      text: "게시물에 위치 정보 넣기",
-      reducer: setAddLocationInfo,
-    },
-  };
+  const { pageTitle, description, item, hyperlink } = useLanguageMode([
+    "pages",
+    "LocationPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="게시물에 위치 정보 넣기"
+      pageTitle={pageTitle}
       backward
-      topContent={
-        <Description text="활성화하면 게시물에 위치 정보를 첨부할 수 있습니다." />
-      }
+      topContent={<Description text={description} />}
       bottomContent={
         <>
           <SettingsContainer item={item} gap={0} />
           {/* 모달창이 뜰 수 있게 변경할 것 */}
           <HyperLink
-            text="게시물에 추가된 모든 위치 정보를 삭제합니다."
+            text={hyperlink}
             path=""
             out={false}
             className={styles.hyperlink}
