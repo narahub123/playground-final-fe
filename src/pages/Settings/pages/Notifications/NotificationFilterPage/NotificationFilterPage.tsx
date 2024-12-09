9d123559745fac495008e17dad3235775d83a939
nara <parknara0213@gmail.com>
2024-12-09 17:55:15 +0900

    Feat: [추가] 언어 설정
    Refs: #107

--- src/pages/Settings/pages/Notifications/NotificationFilterPage/NotificationFilterPage.tsx
+++ src/pages/Settings/pages/Notifications/NotificationFilterPage/NotificationFilterPage.tsx
@@ -1,47 +1,30 @@
+import {
+  SettingsBranchListContainerType,
+  SettingsCheckBoxContainerType,
+} from "@features/settings-setting/types";
 import { SettingsContainer } from "@features/settings-setting/ui/components";
-import {
-  getFindByEmail,
-  getQualityFilter,
-} from "@shared/@common/model/selectors";
-import {
-  setFindByEmail,
-  setQualityFilter,
-} from "@shared/@common/model/slices/settingsSlice";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { Description } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
 
 const NotificationFilterPage = () => {
-  const list = [
-    {
-      type: "checkbox",
-      selector: getQualityFilter,
-      comp: {
-        text: "퀄리티 필터",
-        reducer: setQualityFilter,
-      },
-      description:
-        "선택하면 중복 및 자동 게시물과 같은 콘텐츠가 필터링됩니다. 팔로우 중이거나 최근 대화한 계정의 알림에 적용되지 않습니다.",
-    },
-    {
-      type: "card",
-      comp: {
-        title: "뮤트 상태의 알림",
-        path: "/settings/notifications/advanced_filters",
-      },
-    },
-  ];
+  const { pageTitle, description, list } = useLanguageMode([
+    "pages",
+    "NotificationFilterPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="필터"
+      pageTitle={pageTitle}
       backward
-      topContent={
-        <Description
-          text={"확인하고자 하는 알림과 확인하고 싶지 않은 알림을 선택하세요."}
-        />
-      }
+      topContent={<Description text={description} />}
       bottomContent={
         <>
-          {list.map((item, index) => (
+          {(
+            list as (
+              | SettingsBranchListContainerType
+              | SettingsCheckBoxContainerType
+            )[]
+          ).map((item, index) => (
             <SettingsContainer item={item} key={index} gap={0} />
           ))}
         </>
