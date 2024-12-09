8facfa08636a1e5f3bf04ce63ecbe9fc949bb98b
nara <parknara0213@gmail.com>
2024-12-09 13:12:26 +0900

    Fix: [수정] 값과 기본값을 받는 방법 변경
    Refs: #92

--- src/features/settings-setting/types/data/checkbox.ts
+++ src/features/settings-setting/types/data/checkbox.ts
@@ -1,7 +1,10 @@
 interface CheckBoxType {
   text: string;
-  reducer: (value: boolean) => { type: string; payload: boolean };
-  initialValue: boolean;
+  reducer: (value: boolean | string | number) => {
+    type: string;
+    payload: boolean | string | number;
+  };
+  value: undefined | string | number;
 }
 
 export type { CheckBoxType };
--- src/features/settings-setting/ui/components/CheckBox/CheckBox.tsx
+++ src/features/settings-setting/ui/components/CheckBox/CheckBox.tsx
@@ -1,32 +1,47 @@
-import { Icon } from "@shared/@common/ui/components";
 import styles from "./CheckBox.module.css";
-import { useEffect, useState } from "react";
-import { useAppDispatch } from "@app/store";
+import { Icon } from "@shared/@common/ui/components";
+import { RootState, useAppDispatch } from "@app/store";
 import { CheckBoxType } from "@features/settings-setting/types/data";
+import { useSelector } from "react-redux";
 
 interface CheckBoxProps {
   item: CheckBoxType;
+  selector: (value: RootState) => boolean | string | number;
   isRounded?: boolean;
   className?: string;
 }
 
-const CheckBox = ({ item, isRounded = false, className }: CheckBoxProps) => {
-  const { text, reducer, initialValue } = item;
-
+const CheckBox = ({
+  item,
+  selector, // 기본 값 가져오기 위한 selector
+  isRounded = false,
+  className,
+}: CheckBoxProps) => {
   const dispatch = useAppDispatch();
-  const [isChecked, setIsChecked] = useState(initialValue);
+  const { text, reducer, value } = item;
 
-  useEffect(() => {
-    dispatch(reducer(isChecked));
-  }, [isChecked]);
+  // 기본 값
+  const initialValue = useSelector(selector);
+
+  // 클릭 이벤트
+  const handleClick = (value: undefined | string | number) => {
+    // value 값이 주어지지 않으면(true or false)
+    if (!value) {
+      dispatch(reducer(!initialValue));
+      // value 값이 주어지면
+    } else {
+      dispatch(reducer(value));
+    }
+  };
 
   return (
     <div
       className={`${styles.checkbox} ${className}`}
-      onClick={() => setIsChecked(!isChecked)}
+      onClick={() => handleClick(value)}
     >
       <span>{text}</span>
-      {isChecked ? (
+      {(!value ? initialValue : initialValue === value) ? (
+        // 일치하는 경우
         isRounded ? (
           <Icon
             iconName="roundedCheckedbox"
@@ -42,7 +57,8 @@ const CheckBox = ({ item, isRounded = false, className }: CheckBoxProps) => {
             className={styles.icon}
           />
         )
-      ) : isRounded ? (
+      ) : // 일치하지 않은 경우
+      isRounded ? (
         <Icon iconName="roundedCheckbox" iconTitle="" fontSize={22} />
       ) : (
         <Icon iconName="checkbox" iconTitle="" fontSize={22} />
