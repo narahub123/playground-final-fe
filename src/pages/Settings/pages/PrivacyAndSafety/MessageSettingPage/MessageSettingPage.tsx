36a1e24c2afe794092f953026c28d3420ed88b37
nara <parknara0213@gmail.com>
2024-12-09 15:23:36 +0900

    Feat: [추가] 언어설정
    Refs: #103

--- src/pages/Settings/pages/PrivacyAndSafety/MessageSettingPage/MessageSettingPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/MessageSettingPage/MessageSettingPage.tsx
@@ -1,75 +1,26 @@
+import {
+  SettingsBranchListContainerType,
+  SettingsCheckBoxContainerType,
+} from "@features/settings-setting/types";
 import { SettingsContainer } from "@features/settings-setting/ui/components";
-import {
-  getAllowMessages,
-  getHideMessages,
-  getShowRead,
-} from "@shared/@common/model/selectors/settingsSelectors";
-import {
-  setAllowMessages,
-  setHideMessages,
-  setShowRead,
-} from "@shared/@common/model/slices/settingsSlice";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { MainLayout } from "@shared/pages/layout";
 
 const MessageSettingPage = () => {
-  const list = [
-    {
-      text: "다음 사용자의 쪽지 요청 허용",
-      detail: "내가 팔로우하는 사용자가 보낸 쪽지는 항상 받을 수 있습니다. ",
-      type: "checkboxlist",
-      selector: getAllowMessages,
-      comp: [
-        {
-          text: "받지 않음",
-          reducer: setAllowMessages,
-          value: "none",
-        },
-        {
-          text: "인증 받은 사용자",
-          reducer: setAllowMessages,
-          value: "authenticated",
-        },
-        {
-          text: "받지 않음",
-          reducer: setAllowMessages,
-          value: "all",
-        },
-      ],
-    },
-    {
-      title: "",
-      type: "checkbox",
-      selector: getHideMessages,
-      comp: {
-        text: "저질스러운 쪽지 필터링하기",
-        reducer: setHideMessages,
-      },
-      description:
-        "스팸 또는 저질스러운 내용이 포함된 것으로 파악되는 쪽지 요청을 숨깁니다. 이러한 요청은 쪽지 요청함의 하단에 있는 별도의 받은 쪽지함으로 전송됩니다. 원하는 경우 여전히 확인할 수 있습니다.",
-      top: true,
-      disabled: true,
-    },
-    {
-      title: "",
-      type: "checkbox",
-      selector: getShowRead,
-      comp: {
-        text: "읽음 표시 보기",
-        reducer: setShowRead,
-      },
-      description:
-        "쪽지를 주고 받는 사람들이 내가 메시지를 확인했을 때를 알 수 있도록 허용합니다. 읽음 표시는 쪽지 요청에 나타나지 않습니다.",
-      top: true,
-    },
-  ];
+  const { pageTitle, list } = useLanguageMode(["pages", "MessageSettingPage"]);
   return (
     <MainLayout
-      pageTitle="쪽지"
+      pageTitle={pageTitle}
       backward
       topContent={<></>}
       bottomContent={
         <>
-          {list.map((item, index) => (
+          {(
+            list as (
+              | SettingsBranchListContainerType
+              | SettingsCheckBoxContainerType
+            )[]
+          ).map((item, index) => (
             <SettingsContainer item={item} key={index} gap={0} />
           ))}
         </>
