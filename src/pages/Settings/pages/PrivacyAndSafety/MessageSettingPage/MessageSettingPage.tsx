92ec458e2423e44149867191ac3f3c39383590d5
nara <parknara0213@gmail.com>
2024-12-09 15:09:19 +0900

    Feat: [추가] 수정된 settingsContainer 적용
    Refs: #103

--- src/pages/Settings/pages/PrivacyAndSafety/MessageSettingPage/MessageSettingPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/MessageSettingPage/MessageSettingPage.tsx
@@ -14,7 +14,8 @@ import { MainLayout } from "@shared/pages/layout";
 const MessageSettingPage = () => {
   const list = [
     {
-      title: "다음 사용자의 쪽지 요청 허용",
+      text: "다음 사용자의 쪽지 요청 허용",
+      detail: "내가 팔로우하는 사용자가 보낸 쪽지는 항상 받을 수 있습니다. ",
       type: "checkboxlist",
       selector: getAllowMessages,
       comp: [
@@ -34,7 +35,6 @@ const MessageSettingPage = () => {
           value: "all",
         },
       ],
-      description: "",
     },
     {
       title: "",
@@ -46,6 +46,8 @@ const MessageSettingPage = () => {
       },
       description:
         "스팸 또는 저질스러운 내용이 포함된 것으로 파악되는 쪽지 요청을 숨깁니다. 이러한 요청은 쪽지 요청함의 하단에 있는 별도의 받은 쪽지함으로 전송됩니다. 원하는 경우 여전히 확인할 수 있습니다.",
+      top: true,
+      disabled: true,
     },
     {
       title: "",
@@ -57,6 +59,7 @@ const MessageSettingPage = () => {
       },
       description:
         "쪽지를 주고 받는 사람들이 내가 메시지를 확인했을 때를 알 수 있도록 허용합니다. 읽음 표시는 쪽지 요청에 나타나지 않습니다.",
+      top: true,
     },
   ];
   return (
@@ -67,7 +70,7 @@ const MessageSettingPage = () => {
       bottomContent={
         <>
           {list.map((item, index) => (
-            <SettingsContainer item={item} key={index} />
+            <SettingsContainer item={item} key={index} gap={0} />
           ))}
         </>
       }
