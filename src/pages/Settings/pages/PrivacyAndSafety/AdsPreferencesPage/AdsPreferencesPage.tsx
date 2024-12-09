83bbd9143a0eed97c4d1b33d6f06476c20a537a7
nara <parknara0213@gmail.com>
2024-12-09 16:59:34 +0900

    Feat: [추가] 언어 설정
    Refs: #105

--- src/pages/Settings/pages/PrivacyAndSafety/AdsPreferencesPage/AdsPreferencesPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/AdsPreferencesPage/AdsPreferencesPage.tsx
@@ -2,43 +2,23 @@ import {
   SettingsBranchCard,
   SettingsContainer,
 } from "@features/settings-setting/ui/components";
-import { getAllowBehavioralAds } from "@shared/@common/model/selectors";
-import { setAllowBehavioralAds } from "@shared/@common/model/slices/settingsSlice";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { Description } from "@shared/@common/ui/components";
 import { MainContentListLayout, MainLayout } from "@shared/pages/layout";
 
 const AdsPreferencesPage = () => {
-  const checbox = {
-    type: "checkbox",
-    selector: getAllowBehavioralAds,
-    comp: {
-      text: "맞춤광고",
-      reducer: setAllowBehavioralAds,
-    },
-    description:
-      "PlayGround에서 항상 내 PlayGround 활동에 기반한 광고를 보게 됩니다. 이 설정이 활성화되어 있으면 PlayGround에서 내 PlayGround 활동과 파트너가 제공한 기타 온라인 활동 및 정보를 결합하여 PlayGround 내외에서 PlayGround 광고주의 광고를 맞춤 설정할 수 있습니다.",
-  };
-
-  const branchList = [
-    {
-      title: "관심사",
-      path: "/settings/your_twitter_data/twitter_interests",
-    },
-    {
-      title: "내 광고주 목록",
-      path: "/settings/your_twitter_data/audiences",
-    },
-  ];
+  const { pageTitle, description, checkbox, branchList } = useLanguageMode([
+    "pages",
+    "AdsPreferencesPage",
+  ]);
   return (
     <MainLayout
-      pageTitle="광고 환경설정"
+      pageTitle={pageTitle}
       backward
-      topContent={
-        <Description text={"PlayGround에서 광고 설정을 관리합니다."} />
-      }
+      topContent={<Description text={description} />}
       bottomContent={
         <>
-          <SettingsContainer item={checbox} gap={0} />
+          <SettingsContainer item={checkbox} gap={0} />
           <MainContentListLayout
             list={branchList}
             item={<SettingsBranchCard />}
