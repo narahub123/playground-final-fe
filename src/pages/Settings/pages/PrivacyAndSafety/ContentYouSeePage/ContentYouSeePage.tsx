ee657e3ab69fe521dbd507bd14a920aba6ec4c5f
nara <parknara0213@gmail.com>
2024-12-09 10:01:58 +0900

    Feat: [추가] 언어 설정
    Refs: #102

--- src/pages/Settings/pages/PrivacyAndSafety/ContentYouSeePage/ContentYouSeePage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/ContentYouSeePage/ContentYouSeePage.tsx
@@ -1,50 +1,31 @@
-import { CheckBoxType } from "@features/settings-setting/types/data";
 import {
   CheckBox,
   SettingsBranchCard,
 } from "@features/settings-setting/ui/components";
+import { useLanguageMode } from "@shared/@common/model/hooks";
 import { getUser } from "@shared/@common/model/selectors";
-import { setBackgroundTheme } from "@shared/@common/model/slices/settingsSlice";
 import { Description } from "@shared/@common/ui/components";
 import { MainContentListLayout, MainLayout } from "@shared/pages/layout";
 import { useSelector } from "react-redux";
 
 const ContentYouSeePage = () => {
   const user = useSelector(getUser);
-  const item: CheckBoxType = {
-    text: "민감한 내용을 포함할 수 있는 미디어 표시",
-    reducer: setBackgroundTheme,
-    initialValue: false,
-  };
-  const branchList = [
-    {
-      title: "토픽",
-      path: `/${user.userId}/topics`,
-    },
-    {
-      title: "관심사",
-      path: `/settings/your_data/your_interests`,
-    },
-    {
-      title: "탐색 설정",
-      path: `/settings/explore`,
-    },
-    {
-      title: "검색 설정",
-      path: `/settings/search`,
-    },
-  ];
+  const { pageTitle, description, checkboxItem, branchList } = useLanguageMode([
+    "pages",
+    "ContentYouSeePage",
+  ]);
+
   return (
     <MainLayout
-      pageTitle="표시되는 콘텐츠"
+      pageTitle={pageTitle}
       backward
-      topContent={<Description text />}
+      topContent={<Description text={description} />}
       bottomContent={
         <>
-          <CheckBox item={item} />
+          <CheckBox item={checkboxItem} />
           <MainContentListLayout
             item={<SettingsBranchCard />}
-            list={branchList}
+            list={branchList(user)}
           />
         </>
       }
