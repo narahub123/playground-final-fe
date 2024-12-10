698db57e8d675ed3596d8496dcb858cacc40d92d
nara <parknara0213@gmail.com>
2024-12-10 12:15:14 +0900

    Feat: [추가] input 추가
    Refs: #117

--- src/pages/Settings/pages/Account/GenderPage/GenderPage.tsx
+++ src/pages/Settings/pages/Account/GenderPage/GenderPage.tsx
@@ -2,31 +2,43 @@ import styles from "./GenderPage.module.css";
 import { SettingsContainer } from "@features/settings-setting/ui/components";
 import { getGender } from "@shared/@common/model/selectors";
 import { setGender } from "@shared/@common/model/slices/userSlice";
-import { Description } from "@shared/@common/ui/components";
+import { Description, Input } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
+import { useState } from "react";
 
 const GenderPage = () => {
-  const item = {
-    type: "checkboxlist",
-    selector: getGender,
-    comp: [
-      {
+  const [isOpen, setIsOpen] = useState(false);
+  const list = [
+    {
+      type: "checkbox",
+      selector: getGender,
+      comp: {
         text: "여성",
         reducer: setGender,
         value: "f",
       },
-      {
+    },
+    {
+      type: "checkbox",
+      selector: getGender,
+      comp: {
         text: "남성",
         reducer: setGender,
         value: "m",
       },
-      {
-        text: "성별을 추가하세요",
-        reducer: setGender,
-        value: "new",
-      },
-    ],
+    },
+  ];
+
+  const item = {
+    type: "checkbox",
+    selector: getGender,
+    comp: {
+      text: "성별을 추가하세요.",
+      reducer: setGender,
+      value: "new",
+    },
   };
+
   return (
     <MainLayout
       pageTitle="성별"
@@ -40,7 +52,17 @@ const GenderPage = () => {
       }
       bottomContent={
         <>
-          <SettingsContainer item={item} gap={0} />
+          {list.map((item, index) => (
+            <SettingsContainer item={item} gap={0} key={index} isRounded />
+          ))}
+          <div className={styles.wrapper} onClick={() => setIsOpen(true)}>
+            <SettingsContainer item={item} gap={0} isRounded />
+          </div>
+          {isOpen && (
+            <div className={styles[`input-wrapper`]}>
+              <Input field="gender" fieldTitle="성별" />
+            </div>
+          )}
           <div className={styles[`button-container`]}>
             <button className={styles.button}>저장</button>
           </div>
