a06ab3fc1f4a2bc530c94afd7bb9c76e07be616c
nara <parknara0213@gmail.com>
2024-12-11 12:46:16 +0900

    Feat: [추가] 버튼 역할 추가
    Refs: #99

--- src/shared/@common/ui/components/HyperLink/HyperLink.tsx
+++ src/shared/@common/ui/components/HyperLink/HyperLink.tsx
@@ -3,20 +3,42 @@ import { Link } from "react-router-dom";
 
 interface HyperLinkProps {
   text: string;
-  path: string;
+  path?: string;
   out?: boolean;
   className?: string;
+  setter?: React.Dispatch<React.SetStateAction<any>>;
 }
 
-const HyperLink = ({ text, path, className, out = true }: HyperLinkProps) => {
-  return out ? (
-    <a href={path} className={`${styles.hyperlink} ${className}`}>
-      {text}
-    </a>
+const HyperLink = ({
+  text,
+  path,
+  className,
+  out = true,
+  setter,
+}: HyperLinkProps) => {
+  const handleClick = () => {
+    if (!setter) return;
+    setter(text);
+  };
+  return path ? (
+    out ? (
+      <a href={path} className={`${styles.hyperlink} ${className}`}>
+        {text}
+      </a>
+    ) : (
+      <Link to={path} className={`${styles.hyperlink} ${className}`}>
+        {text}
+      </Link>
+    )
   ) : (
-    <Link to={path} className={`${styles.hyperlink} ${className}`}>
+    <button
+      role="button"
+      tabIndex={0}
+      className={`${styles.hyperlink} ${className}`}
+      onClick={setter ? () => handleClick() : undefined}
+    >
       {text}
-    </Link>
+    </button>
   );
 };
 
