1e3532f01e345f0a2e9cd22f2c5ca065dad1c5e5
nara <parknara0213@gmail.com>
2024-12-14 19:13:07 +0900

    Fix: [수정] Tab props 값 수정
    Refs: #141

--- src/pages/Settings/pages/PrivacyAndSafety/TopicsNotInterestedPage/TopicsNotInterestedPage.tsx
+++ src/pages/Settings/pages/PrivacyAndSafety/TopicsNotInterestedPage/TopicsNotInterestedPage.tsx
@@ -1,10 +1,13 @@
+import { getUser } from "@shared/@common/model/selectors";
 import styles from "./TopicsNotInterestedPage.module.css";
 import { useLanguageMode } from "@shared/@common/model/hooks";
 import { TopicFollowCard } from "@shared/@common/ui/components";
 import { MainLayout } from "@shared/pages/layout";
 import { Tab } from "@shared/pages/ui";
+import { useSelector } from "react-redux";
 
 const TopicsNotInterestedPage = () => {
+  const user = useSelector(getUser);
   const { pageTitle, tabList } = useLanguageMode(["pages", "TopicsPage"]);
 
   const list = ["아이즈원", "강혜원", "김민주"];
@@ -12,7 +15,7 @@ const TopicsNotInterestedPage = () => {
     <MainLayout
       pageTitle={pageTitle}
       backward
-      topContent={<Tab list={tabList} />}
+      topContent={<Tab list={tabList(user.userId)} />}
       bottomContent={
         <ul className={styles.list}>
           {list.map((item, index) => (
